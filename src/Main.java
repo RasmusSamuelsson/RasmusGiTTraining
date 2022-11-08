7261da825955ae6ef4454dda02964fcf0a7ecda8
RasmusSamuelsson <rasmus.samuelsson@gmail.com>
2022-11-08 13:37:47 +0100

    Change number 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hej Rasmus!Change 1 + Change 2");
+        System.out.println("Hej Rasmus!Change 1 + Change 2 + Change in Terminal");
     }
 }

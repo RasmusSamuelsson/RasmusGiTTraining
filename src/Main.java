05f3af4360cb7340011cb957b9053965867e25d4
RasmusSamuelsson <rasmus.samuelsson@gmail.com>
2022-11-08 16:33:57 +0100

    Ännu en rad

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
         System.out.println("Hej Rasmus!Change 4");
         System.out.println("Lägger till en rad här.");
+        System.out.println("Så lägger vi till ännu en rad!");
     }
 }

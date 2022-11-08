705df86f140544f8af7437e8ed304208f8ad77b1
RasmusSamuelsson <rasmus.samuelsson@gmail.com>
2022-11-08 16:25:39 +0100

    La till en rad 'sout'

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hej Rasmus!Change 4");
+        System.out.println("Lägger till en rad här.");
     }
 }
